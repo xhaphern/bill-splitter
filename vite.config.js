5020a553017abe691beb206043d4cbc748387a5c
Ahmed Shaffan <ah.shahfan@outlook.com>
2025-08-25 22:17:58 +0500

    fix: set correct base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/bill-splitter/'
+  base: '/bill-splitter/',   // 👈 important for GitHub Pages
 })
